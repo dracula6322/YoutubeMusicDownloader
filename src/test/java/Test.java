381500b5277b08731837e289c226e830db3a06ec
Andrey <and.solomatin@mail.ru>
2020-02-25 00:13:36 +0300

    Before refactor commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -6,17 +6,6 @@ public class Test {
 
   @org.junit.Test
   public void qew(){
-    System.out.println("\"Hello\" = " + "Hello");
-
-    ArrayList<Pair<String, String>> pairs = new ArrayList<>();
-    pairs.add(new Pair<>("asd1", "zxc"));
-    pairs.add(new Pair<>("asd2", "zxc (2)"));
-    pairs.add(new Pair<>("asd3", "zxc"));
-
-    pairs = YoutubeDownloader.findEqualsName(pairs);
-    for (Pair<String, String> pair : pairs) {
-      System.out.println("pair = " + pair);
-    }
   }
 
 }
