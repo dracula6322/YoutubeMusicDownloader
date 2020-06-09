a15ce7ac843cea74863a77c072e40ae6de43d159
Andrey <and.solomatin@mail.ru>
2020-06-09 22:42:52 +0300

    Add ProgramExecutor bean

--- src/main/java/com/green/square/ProgramExecutorSpring.java
+++ src/main/java/com/green/square/ProgramExecutorSpring.java
@@ -1,7 +1,9 @@
 package com.green.square;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProgramExecutorSpring {
 
   @Bean
